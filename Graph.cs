8293b437c52688b0d4e43ad622116da8546a1d8f
wirasuta <wirasutat@gmail.com>
2019-03-11 10:37:23 +0700

    Init Commit. Added Graph Class

---
+++ Graph.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Stima{ 
+    public class Graph{
+        private List<int>[] adjList;
+        private int n;
+        public Graph(int _n){
+            //Ctor _n simpul tanpa sisi
+            //Inisialisasi sebanyak n+1 anggota, elemen ke-0 tidak digunakan
+            //adjList[v] berisi List simpul w yang tersambung dengan v
+            n = _n;
+            adjList = new List<int>[n+1];
+            for (int i = 0; i <= n; i++){
+                adjList[i] = new List<int>();
+            }
+        }
+        public Graph(string filename){
+            //Ctor dengan membaca file external
+            string[] lines = System.IO.File.ReadAllLines(filename);
+            n = int.Parse(lines[0]);
+            adjList = new List<int>[n+1];
+            for (int i = 0; i <= n; i++){
+                adjList[i] = new List<int>();
+            }
+            foreach (string line in lines.Skip(1).ToArray()){
+                string[] vw = line.Split(' ');
+                addEdge(int.Parse(vw[0]),int.Parse(vw[1]));
+            }
+        }
+        public void addEdge(int v, int w){
+            //Prekondisi : v dan w ada pada graph
+            adjList[v].Add(w);
+            adjList[w].Add(v);
+        }
+        public void removeEdge(int v, int w){
+            //Prekondisi : v dan w ada pada graph
+            adjList[v].Remove(w);
+            adjList[w].Remove(v);
+        }
+        public int getNodeCount(){
+            return n;
+        }
+        public List<int> getAdjNodes(int v){
+            //Prekondisi : v ada pada graph
+            return adjList[v];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stima{
+    class Program{
+        static void Main(string[] args)
+        {
+            if (args.Length == 0){
+                Console.WriteLine("Usage : dotnet run <filename>");
+                System.Environment.Exit(-1);
+            }else{
+                Console.WriteLine("Input File : {0}",args[0]);
+            }
+            Graph G1 = new Graph(args[0]);
+            for (int i = 1; i <= G1.getNodeCount(); i++){
+                List<int> adj = G1.getAdjNodes(i);
+                Console.Write("{0} -> ",i);
+                foreach (int item in adj){
+                    Console.Write("{0} ",item);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
