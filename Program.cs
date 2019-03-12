831851610132297c6d723bd6a48e7a800f9d4024
wirasuta <wirasutat@gmail.com>
2019-03-13 03:47:14 +0700

    Melengkapi graph class

--- Program.cs
+++ Program.cs
@@ -12,17 +12,7 @@ namespace PetakUmpetAntahBerantah{
                 Console.WriteLine("Input File : {0}",args[0]);
             }
             Graph G1 = new Graph(args[0]);
-            for (int i = 1; i <= G1.getNodeCount(); i++){
-                List<int> adj = G1.getAdjNodes(i);
-                Console.Write("{0} -> ",i);
-                foreach (int item in adj){
-                    Console.Write("{0} ",item);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("{0} :: {1}",5,DFS.Search(G1,0,4,5));
-            Console.WriteLine("{0} :: {1}",1,DFS.Search(G1,0,4,1));
-            Console.WriteLine("{0} :: {1}",9,DFS.Search(G1,0,4,9));
+            G1.print();
         }
     }
 }
