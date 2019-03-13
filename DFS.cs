087354a722988a7eb1213a83f181372d7a338f73
Asif Hummam Rais <hashshura@gmail.com>
2019-03-13 10:47:33 +0700

    Change DFS

--- DFS.cs
+++ DFS.cs
@@ -1,20 +1,30 @@
 using System.Collections.Generic;
 
 namespace PetakUmpetAntahBerantah{
-    public static class DFS{
-        public static bool Search(Graph G, int prev, int current, int target){
-            List<int> connectedVert = G.getAdjNodes(current);
-            bool found = false;
-            connectedVert.Remove(prev);
-            foreach (int vert in connectedVert){
-                if (vert == target){
-                    found = true;
-                    break;
-                }else{
-                    found = found || Search(G,current,vert,target);
-                }
+    public static class DFS {
+        private static bool[] visited;
+        private static int time;
+
+        public static void setDatangPergi(Graph G){
+            time = 0;
+            n = G.getNodeCount() + 1;
+            visited = new bool[n];
+            for (int i = 0; i <= n; i++)
+                visited[i] = false;
+            dfs(G, 1);
+        }
+
+        public static void dfs(Graph G, int current){
+            if (!visited[current]){
+                time++;
+                G.setDatang(current, time);
+                visited[current] = true;
+                List<int> connectedVert = G.getAdjNodes(current);
+                foreach (int vert in connectedVert)
+                    dfs(G, vert);
+                time++;
+                G.setPergi(current, time);
             }
-            return found;
         }
     }
 }
